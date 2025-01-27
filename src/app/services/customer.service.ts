bb9df03608f9b02fd14722000a328a7871a534bd
Nory9 <noormisk94@gmail.com>
2025-01-28 00:41:40 +0300

    alomost  done

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -2,33 +2,34 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Customer } from '../model/customer.type';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CustomerService {
-  private apiUrl = 'http://localhost:8080/api/customers'; 
+  
 
   constructor(private http: HttpClient) {}
 
   getCustomers(): Observable<Customer[]> {
-    return this.http.get<Customer[]>(this.apiUrl);
+    return this.http.get<Customer[]>(`${environment.API_URL}/customers`);
   }
 
   getCustomerById(id: number): Observable<Customer> {
-    return this.http.get<Customer>(`${this.apiUrl}/${id}`);
+    return this.http.get<Customer>(`${environment.API_URL}/customers/${id}`);
   }
 
   deleteCustomer(customerId: number): Observable<string> {
-    return this.http.delete(`${this.apiUrl}/${customerId}`, { responseType: 'text' });
+    return this.http.delete(`${environment.API_URL}/customers/${customerId}`, { responseType: 'text' });
   }
 
   addCustomer(customer: any): Observable<any> {
-    return this.http.post('http://localhost:8080/api/customers', customer,{responseType:'text'});
+    return this.http.post(`${environment.API_URL}/customers`, customer,{responseType:'text'});
   }
 
   updateCustomer(customerId: number, customer: Customer): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${customerId}`, customer, { responseType: 'text' });
+    return this.http.put(`${environment.API_URL}/customers/${customerId}`, customer, { responseType: 'text' });
   }
   
   
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Order } from '../model/order.type';
+import { environment } from '../environments/environment';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrderService {
-  private apiUrl = 'http://localhost:8080/api/orders'; 
 
   constructor(private http: HttpClient) {}
 
   
   getOrdersByCustomerId(customerId: number): Observable<Order[]> {
     console.log(customerId);
-    return this.http.get<Order[]>(`${this.apiUrl}/${customerId}`);
+    return this.http.get<Order[]>(`${environment.API_URL}/orders/${customerId}`);
   }
 }
